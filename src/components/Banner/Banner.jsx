cb55e570190eb3e78cf3dc78f320bc71ff86b00c
hellosiyam <hellosiyambd@gmail.com>
2025-03-15 15:56:06 +0600

    daily-gear-shop

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -5,7 +5,7 @@ const Banner = () => {
             {/* min-h-screen */}
             <div className="bg-[#9538E2] rounded-lg lg:rounded-2xl">
                 <div className="text-center py-20 lg:py-28 flex flex-col gap-4 lg:gap-8 ">
-                    <h1 className="sora text-xl lg:text-5xl lg:w-2/3 mx-auto text-white font-bold max-md:px-4">Upgrade Your Tech Accessorize with Gadget Heaven Accessories</h1>
+                    <h1 className="sora text-2xl lg:text-5xl lg:w-2/3 mx-auto text-white font-bold max-md:px-4">Upgrade Your Tech Accessorize with Gadget Heaven Accessories</h1>
                     <div className="px-4">
                         <p className="text-lg lg:text-2xl font-normal text-white lg:w-2/4 mx-auto">Explore the latest gadgets that will take your experience to the next level. From smart devices to the coolest accessories, we have it all!</p>
                     </div>
